9e3787b5fe4fe4d9b2ade52c74231a0ac86a8fad
Marc Alexandru Pop <marcpop2003@yahoo.com>
2020-09-08 17:03:42 +0300

    Snake is spawning and moving upwards

--- SnakeMVC/wwwroot/Views/Snake/Play.js
+++ SnakeMVC/wwwroot/Views/Snake/Play.js
@@ -3,9 +3,12 @@
     var width = $('#gridWidth').val();
 
     $('#submitButton').on("click", function () {
-        
+
         createGrid(height, width);
         spawnSnake();
+        setInterval(function () {
+            moveSnake();
+        }, 750);
     });
 
 
@@ -16,26 +19,56 @@
         $('#table').append('<table id="grid"></table')
 
         for (i = 0; i < height; i++) {
-            $('#grid').append('<tr class="gridRow-' + i + '"></tr>');
+            $('#grid').append('<tr class="gridRow ' + i + '"></tr>');
             for (j = 0; j < width; j++) {
-                $('.gridRow-' + i +'').append('<td class="gridCol-' + j + '"></td>');
+                $('.gridRow.' + i + '').append('<td class="gridCol ' + j + '"></td>');
 
             }
         }
-        
+
 
         $('#gridInput').remove();
 
     }
 
+    var snakeHeadX = 12;
+    var snakeHeadY = 12;
+
+    var snakeTailX = 12;
+    var snakeTailY = 15;
 
     function spawnSnake() {
 
-        $('.gridRow 12').children('.gridCol 12').addClass("snake");
-        $('.gridRow 13').children('.gridCol 12').addClass('snake');
-        $('.gridRow 14').children('.gridCol 12').addClass('snake');
-        $('.gridRow 15').children('.gridCol 12').addClass('snake');
+        $('.gridRow.' + snakeHeadY + '').children('.gridCol.' + snakeHeadX + '').addClass("snake");
+        $('.gridRow.13').children('.gridCol.12').addClass('snake');
+        $('.gridRow.14').children('.gridCol.12').addClass('snake');
+        $('.gridRow.' + snakeTailY + '').children('.gridCol.' + snakeTailX + '').addClass('snake');
+
 
     }
+
+    function moveSnake() {
+
+        if (snakeHeadY > 0) {
+            //debugger;
+            snakeHeadY--;
+            $('.gridRow.' + snakeHeadY + '').children('.gridCol.' + snakeHeadX + '').addClass("snake");
+            $('.gridRow.' + snakeTailY + '').children('.gridCol.' + snakeTailX + '').removeClass('snake');
+
+            if (snakeTailY > 0) {
+                snakeTailY--;
+            }
+            else if (snakeTailY === 0) {
+                snakeTailY = height - 1;
+            }
+
+        }
+        if (snakeHeadY <= 0) {
+            snakeHeadY = height;
+        }
+
+    }
+
+
 
 })
