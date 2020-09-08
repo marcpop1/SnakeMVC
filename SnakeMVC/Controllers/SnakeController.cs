f7397206d18c6d92eb8b6ac50a82bf44ec539883
Marc Alexandru Pop <marcpop2003@yahoo.com>
2020-09-08 17:01:01 +0300

    Add project files.

---
+++ SnakeMVC/Controllers/SnakeController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SnakeMVC.Controllers
+{
+    public class SnakeController : Controller
+    {
+        public IActionResult Play()
+        {
+            return View();
+        }
+    }
+}
---
+++ SnakeMVC/wwwroot/Views/Snake/Play.js
@@ -0,0 +1,41 @@
+﻿$(document).ready(function () {
+    var height = $('#gridHeight').val();
+    var width = $('#gridWidth').val();
+
+    $('#submitButton').on("click", function () {
+        
+        createGrid(height, width);
+        spawnSnake();
+    });
+
+
+    function createGrid(height, width) {
+
+        $('#grid').remove();
+
+        $('#table').append('<table id="grid"></table')
+
+        for (i = 0; i < height; i++) {
+            $('#grid').append('<tr class="gridRow-' + i + '"></tr>');
+            for (j = 0; j < width; j++) {
+                $('.gridRow-' + i +'').append('<td class="gridCol-' + j + '"></td>');
+
+            }
+        }
+        
+
+        $('#gridInput').remove();
+
+    }
+
+
+    function spawnSnake() {
+
+        $('.gridRow 12').children('.gridCol 12').addClass("snake");
+        $('.gridRow 13').children('.gridCol 12').addClass('snake');
+        $('.gridRow 14').children('.gridCol 12').addClass('snake');
+        $('.gridRow 15').children('.gridCol 12').addClass('snake');
+
+    }
+
+})
