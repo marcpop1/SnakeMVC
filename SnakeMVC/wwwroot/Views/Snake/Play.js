91a34a048c3dcbdfbf65a2d8a24592bf8f42f525
Home <Home@DESKTOP-MP7D4SO>
2020-09-10 16:28:55 +0300

    We track the snakes whole body

--- SnakeMVC/wwwroot/Views/Snake/Play.js
+++ SnakeMVC/wwwroot/Views/Snake/Play.js
@@ -12,11 +12,13 @@
         //    moveSnakeNew();
         //}, 750);
 
-        setInterval(function () {
-            $(document).on("keydown", function (event) {
-                snakeController();
-            }); 
-        }, 750);
+        $(document).on("keydown", function (event) {
+            snakeController();
+        }); 
+
+        //setInterval(function () {
+            
+        //}, 750);
 
     });
 
@@ -80,7 +82,10 @@
 
     }
 
-    function moveSnakeNew() {
+    var snakeBodyX;
+    var snakeBodyY;
+
+    function moveSnakeNew(input) {
 
         //$(document).on("keydown", function (event) {
         //    var pressedKey = event.key;
@@ -104,9 +109,69 @@
         //    }
 
         //});
+        //debugger;
+        //snakeHeadY--;
 
-        $('.gridRow.' + snakeHeadY + '').children('.gridCol.' + snakeHeadX + '').addClass("snake");
-        $('.gridRow.' + snakeTailY + '').children('.gridCol.' + snakeTailX + '').removeClass('snake');            
+        if (input == 1) {
+            snakeHeadY--;
+        }
+        else if (input == 2) {
+            snakeHeadX--;
+        }
+        else if (input == 3) {
+            snakeHeadY++;
+        }
+        else if (input == 4) {
+            snakeHeadX++;
+        }
+        else if (input == null) {
+
+        }
+
+        $('.gridRow.' + snakeHeadY + '').children('.gridCol.' + snakeHeadX + '').addClass("snake 1");
+
+        snakeBodyX = snakeHeadX;
+        snakeBodyY = snakeHeadY;
+
+        for (i = 1; i < snakeLength; i++) {
+
+            if (snakeBodyY < 0) {
+                snakeBodyY = height - 1;
+            }
+            else if (snakeBodyY > (height - 1)) {
+                snakeBodyY = 0;
+            }
+            else if (snakeBodyX < 0) {
+                snakeBodyX = width - 1;
+            }
+            else if (snakeBodyX > (width - 1)) {
+                snakeBodyX = 0;
+            }
+
+            if ($('.gridRow.' + (checkLimit(snakeBodyY - 1, height)) + '').children('.gridCol.' + snakeBodyX + '').hasClass('snake ' + i + '')) {
+                $('.gridRow.' + (checkLimit(snakeBodyY - 1, height)) + '').children('.gridCol.' + snakeBodyX + '').removeClass('' + i + '');
+                $('.gridRow.' + (checkLimit(snakeBodyY - 1, height)) + '').children('.gridCol.' + snakeBodyX + '').addClass('' + (i + 1) + '');
+                snakeBodyY--;
+            }
+            else if ($('.gridRow.' + (checkLimit(snakeBodyY + 1, height)) + '').children('.gridCol.' + snakeBodyX + '').hasClass('snake ' + i + '')) {
+                $('.gridRow.' + (checkLimit(snakeBodyY + 1, height)) + '').children('.gridCol.' + snakeBodyX + '').removeClass('' + i + '');
+                $('.gridRow.' + (checkLimit(snakeBodyY + 1, height)) + '').children('.gridCol.' + snakeBodyX + '').addClass('' + (i + 1) + '');
+                snakeBodyY++;
+            }
+            else if ($('.gridRow.' + snakeBodyY + '').children('.gridCol.' + (checkLimit(snakeBodyX - 1, width)) + '').hasClass('snake ' + i + '')) {
+                $('.gridRow.' + (snakeBodyY) + '').children('.gridCol.' + (checkLimit(snakeBodyX - 1, width)) + '').removeClass('' + i + '');
+                $('.gridRow.' + (snakeBodyY) + '').children('.gridCol.' + (checkLimit(snakeBodyX - 1, width)) + '').addClass('' + (i + 1) + '');
+                snakeBodyX--;
+            }
+            else if ($('.gridRow.' + snakeBodyY + '').children('.gridCol.' + (checkLimit(snakeBodyX + 1, width)) + '').hasClass('snake ' + i + '')) {
+                $('.gridRow.' + (snakeBodyY) + '').children('.gridCol.' + (checkLimit(snakeBodyX + 1, width)) + '').removeClass('' + i + '');
+                $('.gridRow.' + (snakeBodyY) + '').children('.gridCol.' + (checkLimit(snakeBodyX + 1, width)) + '').addClass('' + (i + 1) + '');
+                snakeBodyX++;
+            }
+        }
+        
+
+        $('.gridRow.' + snakeTailY + '').children('.gridCol.' + snakeTailX + '').removeClass('snake ' + snakeLength + '');            
         
         if (snakeHeadY <= 0) {
             snakeHeadY = height;
@@ -121,16 +186,16 @@
             snakeHeadX = 0;
         }
 
-        if ($('.gridRow.' + (snakeTailY-1) + '').children('.gridCol.' + snakeTailX + '').hasClass('snake')) {
+        if ($('.gridRow.' + (snakeTailY - 1) + '').children('.gridCol.' + snakeTailX + '').hasClass('snake ' + snakeLength + '')) {
             snakeTailY--;
         }
-        else if ($('.gridRow.' + (snakeTailY + 1) + '').children('.gridCol.' + snakeTailX + '').hasClass('snake')) {
+        else if ($('.gridRow.' + (snakeTailY + 1) + '').children('.gridCol.' + snakeTailX + '').hasClass('snake ' + snakeLength + '')) {
             snakeTailY++;
         }
-        else if ($('.gridRow.' + snakeTailY + '').children('.gridCol.' + (snakeTailX - 1) + '').hasClass('snake')) {
+        else if ($('.gridRow.' + snakeTailY + '').children('.gridCol.' + (snakeTailX - 1) + '').hasClass('snake ' + snakeLength + '')) {
             snakeTailX--;
         }
-        else if ($('.gridRow.' + snakeTailY + '').children('.gridCol.' + (snakeTailX + 1) + '').hasClass('snake')) {
+        else if ($('.gridRow.' + snakeTailY + '').children('.gridCol.' + (snakeTailX + 1) + '').hasClass('snake ' + snakeLength + '')) {
             snakeTailX++;
         }
         else if (snakeTailY === 0) {
@@ -148,35 +213,55 @@
 
     }
 
+    function checkLimit(input, limit) {
+        if (input < 0) {
+            input = limit - 1;
+        }
+        else if (input > (limit - 1)) {
+            input = 0;
+        }
+
+        return input;
+
+    }
+
     
     function snakeController() {
 
         var pressedKey = event.key;
+        var snakeMovement = setInterval(function () {
+            moveSnakeNew();
+        }, 750);
+        debugger;
 
         switch (pressedKey) {
             case "w" || "W":
-                setInterval(function () {
-                    snakeHeadY--;
-                    moveSnakeNew();
+                clearInterval(snakeMovement);
+                snakeMovement = setInterval(function () {
+                    moveSnakeNew(1);
                 }, 750);
+                
                 break;
             case "a" || "A":
-                setInterval(function () {
-                    snakeHeadX--;
-                    moveSnakeNew();
+                clearInterval(snakeMovement);
+                snakeMovement = setInterval(function () {
+                    moveSnakeNew(2);
                 }, 750);
+                
                 break;
             case "s" || "S":
-                setInterval(function () {
-                    snakeHeadY++;
-                    moveSnakeNew();
+                clearInterval(snakeMovement);
+                snakeMovement = setInterval(function () {
+                    moveSnakeNew(3);
                 }, 750);
+                
                 break;
             case "d" || "D":
-                setInterval(function () {
-                    snakeHeadX++;
-                    moveSnakeNew();
+                clearInterval(snakeMovement);
+                snakeMovement = setInterval(function () {
+                    moveSnakeNew(4);
                 }, 750);
+                
                 break;
             //default:
             //    setInterval(function () {
@@ -187,6 +272,44 @@
         }
     }
 
+    function snakeControllerNew() {
+
+        var pressedKey = event.key;
+
+        var snakeMovement;
+
+        if (pressedKey == "w" || pressedKey == "W") {
+            clearInterval(snakeMovement);
+            snakeMovement = setInterval(function () {
+                snakeHeadY--;
+                moveSnakeNew();
+            }, 750);
+        }
+        else if (pressedKey == "a" || pressedKey == "A") {
+            clearInterval(snakeMovement);
+            snakeMovement = setInterval(function () {
+                snakeHeadX--;
+                moveSnakeNew();
+            }, 750);
+        }
+        else if (pressedKey == "s" || pressedKey == "S") {
+            clearInterval(snakeMovement);
+            snakeMovement = setInterval(function () {
+                snakeHeadY++;
+                moveSnakeNew();
+            }, 750);
+        }
+        else if (pressedKey == "d" || pressedKey == "D") {
+            clearInterval(snakeMovement);
+            snakeMovement = setInterval(function () {
+                snakeHeadX++;
+                moveSnakeNew();
+            }, 750);
+        }
+
+        //moveSnakeNew();
+
+    }
 
 
 })
